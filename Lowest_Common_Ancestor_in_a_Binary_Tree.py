43939fa12510b7147fd0d7f5bb8ed797b93d04d1
Ash Dehghan <ash.dehghan@gmail.com>
2018-03-27 22:31:11 -0400

    Working on the common ancestor.

--- Lowest_Common_Ancestor_in_a_Binary_Tree.py
+++ Lowest_Common_Ancestor_in_a_Binary_Tree.py
@@ -4,47 +4,65 @@ Given a binary tree (not a binary search tree) and two values say n1 and n2, wri
 Look at wiki for full definition of Common Ancestor.
 """
 
-
+"""
+	This is the node class.
+	Basically, the tree, is jusr a collection of nodes.
+"""
 class Node:
-	def __init__(self, key):
-		self.key = key 
+	def __init__(self, data):
+		self.data = data 
 		self.left = None
 		self.right = None
-	 
+###################################################
 
-def findLCA(root, n1, n2):
-	 
-	# Base Case
-	if root is None:
-		return None
+
+"""
+	The following method works in the following way:
+	- Each node has a left arm and a right arm.
+	- For each arm of the node, we consider the value of the child node.
+		- If the value of the child node is None (leaf condition), the value of the arm is set to None.
+		- If the value of the child node is equal to one of the nodes we are looking for, then the value of the arm is equal to the value of the child node.
+		- Else, look at the left and right arms of the child node. The value going back up will depend on the values of the left and right arm of the child node.
+"""
+def FindCommonAncestor(NodeObj, val_1, val_2):
+
+	# If Node Object is node, either:
+	# We have a empty tree, or we are the leaf node.
+	if NodeObj is None:
+		return None;
  
-	# If either n1 or n2 matches with root's key, report
-	#  the presence by returning root (Note that if a key is
-	#  ancestor of other, then the ancestor key becomes LCA
-	if root.key == n1 or root.key == n2:
-		return root 
+
+	# If the Node we are looking at matches one of the values we are looking for, return.
+	# In this case, we have two possibility:
+	# We are at the root, and return will exit the function
+	# We are at the sub-node, in which case the recursive function move down the stack, setting one of LeftArm or RightArm equaling the current Node.
+	if (NodeObj.data == val_1 or NodeObj.data == val_2):
+		return NodeObj 
  
-	# Look for keys in left and right subtrees
-	left_lca = findLCA(root.left, n1, n2) 
-	right_lca = findLCA(root.right, n1, n2)
- 
-	# If both of the above calls return Non-NULL, then one key
-	# is present in once subtree and other is present in other,
-	# So this node is the LCA
-	if left_lca and right_lca:
-		return root 
+	# If we are here, it means that the current Node Object is not one of the values we are looking for, so look into the left and right arm of the current node.
+	# When the recursion comes back up to this point, the left arm will hold either None, meaning the value we are looking for does not exists in the left sub tree 
+	# of the current node, or the value of the node, which matches one of the values we are looking for.
+	LeftArm = FindCommonAncestor(NodeObj.left, val_1, val_2);
+	
+	# Same as above, but for the right arm.
+	RightArm = FindCommonAncestor(NodeObj.right, val_1, val_2)
+ 	
+
+	# At this point, for the current node, we have three options.
+	if LeftArm and RightArm:
+		return NodeObj 
  
 	# Otherwise check if left subtree or right subtree is LCA
-	return left_lca if left_lca is not None else right_lca
+	return LeftArm if LeftArm is not None else RightArm
  
  
  
 root = Node(1)
 root.left = Node(2)
-root.right = Node(3)
+root.right = Node(7)
 root.left.left = Node(4)
 root.left.right = Node(5)
-root.right.left = Node(6)
-root.right.right = Node(7)
-print "LCA(4,5) = ", findLCA(root, 4, 5).key
+root.right.left = Node(8)
+root.right.right = Node(12)
+FindCommonAncestor(root, 5, 8)
  
