4b2a31e36c213bf36335e793cf71b6ea2b985e91
Ash Dehghan <ash.dehghan@gmail.com>
2018-03-30 14:21:25 -0400

    Working on two problems related to singly link list.

---
+++ Mth_Element_to_Last_In_Singly_Linked_List.py
@@ -0,0 +1,10 @@
+"""
+	Given a singly linked list, devise a time- and space-efficient algorithm to find the mth-to-last element of the list.
+	Implement your algorithm, taking care to handle relevant error conditions.
+	Define mth to last such that when m = 0 the last element of the list is returned.
+	Note here that we are measuring the distance to the last element. In a singly link list, we can only traverse in 1 direction. 
+
+	Notes:
+	Here are few ways we can look at this problem, remembering we need to save time and space.
+
+"""
---
+++ Stack_Implementation_Using_Singly_Linked_List.py
@@ -0,0 +1,9 @@
+"""
+	In this practice problem, we will be implementing a Stack model, using singly linked list.
+	The Stack is a Firts-In-Last_Out model, so we want to write a class which will have the following properties:
+	- Push method
+	- Pop method
+	- Create Stack method
+	- Delete Stack method 
+"""
+
